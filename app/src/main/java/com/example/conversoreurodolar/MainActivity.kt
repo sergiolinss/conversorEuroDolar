60864dfc86101a4fd5f516c0183692df2e3b91e7
sergiolinss <Sergiovictor2000outlook.com>
2023-12-25 15:41:51 -0300

    Commit Inicial.

---
+++ app/src/main/java/com/example/conversoreurodolar/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.conversoreurodolar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.conversoreurodolar.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonConverter.setOnClickListener {
+            val euros : Double = binding.editEuro.text.toString().toDouble()
+            val dolares = String.format("%.2f",euros * 0.8)
+
+            binding.textDolares.text = "$ $dolares"
+        }
+
+
+    }
+}
